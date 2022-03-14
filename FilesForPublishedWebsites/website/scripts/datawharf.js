2a46242b070acad9ebcc07ed857045e4863eb5d0
Thomas Goldschmidt <491711+tgold@users.noreply.github.com>
2022-03-14 14:18:13 +0100

    several ui and tree enhancements
    - separate package for "uncontained entities"
    - removed "card" div where unnecessary
    - tables that are shown with no sidebar are now using col-auto for layout
    - tree shows datatypes

---
+++ FilesForPublishedWebsites/website/scripts/datawharf.js
@@ -0,0 +1,52 @@
+function modifyLeafNodes(nodes){
+  for(var i = 0, length = nodes.length; i < length; i++){
+    if(!nodes[i].nodes || nodes[i].nodes.length === 0){
+      nodes[i].nodes = null;
+    }else{
+      modifyLeafNodes(nodes[i].nodes);
+    }
+  }
+}
+
+function buildTree(tree, item) {
+   if (tree) {
+    if (item) { 
+        for (var i=0; i<tree.length; i++) { 
+            if (String(tree[i].id) === String(item.parentId)) { 
+                tree[i].nodes.push(item); 
+                break;
+            }
+            else  { buildTree(tree[i].nodes, item); }
+        }
+      }
+      else { 
+        var idx = 0;
+        while (idx < tree.length) { 
+            if (tree[idx].parentId) { buildTree(tree, tree.splice(idx, 1)[0]) } 
+            else { idx++; }
+        }
+      }
+    }
+};
+
+function buildDTTree(tree, item) {
+   if (tree) {
+    if (item) { 
+        for (var i=0; i<tree.length; i++) { 
+            if (String(tree[i].id) === String(item.parentId)) { 
+                tree[i].nodes.push(item); 
+                tree[i].icon = "bi bi-code-slash"; 
+                break;
+            }
+            else  { buildDTTree(tree[i].nodes, item); }
+        }
+      }
+      else { 
+        var idx = 0;
+        while (idx < tree.length) { 
+            if (tree[idx].parentId) { buildDTTree(tree, tree.splice(idx, 1)[0]) } 
+            else { idx++; }
+        }
+      }
+    }
+};
